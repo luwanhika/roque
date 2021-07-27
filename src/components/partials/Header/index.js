d3a7e29ec3b0b847611a0de67674bfb242984021
Luwa <luwa@MacBook-Pro-de-Luwa.local>
2021-07-26 23:31:23 -0300

    create auth  settings

--- src/components/partials/Header/index.js
+++ src/components/partials/Header/index.js
@@ -1,10 +1,53 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import { HeaderArea } from './styled';
 
+import { isLogged } from '../../../helpers/AuthHandler';
+
 const Header = () =>{
+  let logged = isLogged();
+
   return (
     <HeaderArea>
-      ...
+      <div className="container">
+        <div className="logo">
+          <Link to="/">
+            <span className="logo-1">R</span>
+            <span className="logo-2">O</span>
+            <span className="logo-3">Q</span>
+          </Link>
+        </div>
+        <nav>
+          <ul>
+            {logged &&
+              <>
+                <li>
+                  <Link to="/my-account">Minha Conta</Link>
+                </li>
+                <li>
+                  <Link to="/logout">Sair</Link>
+                </li>
+                <li>
+                  <Link to="/post-an-ad" className="button">Poste um anúncio</Link>
+                </li>
+              </>
+            }
+            {!logged &&
+              <>
+                <li>
+                  <Link to="/signin">Login</Link>
+                </li>
+                <li>
+                  <Link to="/signup">Cadastrar</Link>
+                </li>
+                <li>
+                  <Link to="/signin" className="button">Poste um anúncio</Link>
+                </li>
+              </>
+            }
+          </ul>
+        </nav>
+      </div>
     </HeaderArea>
   );
 }
